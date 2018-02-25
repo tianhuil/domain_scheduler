9828f9ff625d03a900b615bdb6914d1413741d5f
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-02-24 20:36:29 -0500

    Domain Scheduler

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='domain_scheduler',
+      version='0.1',
+      description='A domain-aware scheduler for Scrapy that round robin cycles through the domains as it outputs requests to reduce out the load on servers and accelerate scraping',
+      url='http://github.com/tianhuil/domain_scheduler',
+      author='Tianhui Michael Li',
+      license='MIT',
+      packages=['domain_scheduler'],
+      zip_safe=False)
